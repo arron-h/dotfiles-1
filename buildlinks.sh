b92a56b849149c0e8b6f913d33e4b4beaca448f5
Scott Walsh <scottkwalsh@gmail.com>
2012-01-17 16:23:26 +0000

    Symlink full vim folder rather than moving bits and pieces around, pathogen installed

--- buildlinks.sh
+++ buildlinks.sh
@@ -9,7 +9,9 @@ ln -s .dotfiles/.gitconfig .gitconfig
 ln -s .dotfiles/.hgignore .hgignore
 ln -s .dotfiles/.hgrc .hgrc
 ln -s .dotfiles/.screenrc .screenrc
-ln -s .dotfiles/vim/.vimrc .vimrc
+ln -s .dotfiles/.vimrc .vimrc
+ln -s .dotfiles/vim .vim
 
-cp .dotfiles/vim/colors/wombat.vim .vim/colors/wombat.vim
-cp .dotfiles/vim/colors/wombat256.vim .vim/colors/wombat256.vim
+# symlink full vim folder rather than individual color files
+# cp .dotfiles/vim/colors/wombat.vim .vim/colors/wombat.vim
+# cp .dotfiles/vim/colors/wombat256.vim .vim/colors/wombat256.vim
